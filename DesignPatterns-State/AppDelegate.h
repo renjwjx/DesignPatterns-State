4d85c82aaab4e50ac4c2f18a08bf9b71f9ce160f
renjinwei <renjwjx@gmail.com>
2018-05-06 21:34:49 +0800

    Initial Commit

---
+++ DesignPatterns-State/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DesignPatterns-State
+//
+//  Created by jinren on 06/05/2018.
+//  Copyright © 2018 jinren. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ DesignPatterns-State/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  DesignPatterns-State
+//
+//  Created by jinren on 06/05/2018.
+//  Copyright © 2018 jinren. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
